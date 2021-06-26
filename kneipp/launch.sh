55240b9b67636da1f0ed2956cdceeb541daec663
engeir <eirroleng@gmail.com>
2021-06-26 21:49:12 +0200

    Added my own kneipp flavor

---
+++ kneipp/launch.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar top -c $(dirname $0)/config.ini &
+#polybar -c ~/.config/polybar/config.ini external &
+
+my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
+if [[ $my_laptop_external_monitor = *connected* ]]; then
+    polybar external -c $(dirname $0)/config.ini external &
+fi
+
+my_laptop_external_monitor=$(xrandr --query | grep 'DP-2')
+if [[ $my_laptop_external_monitor = *connected* ]]; then
+    polybar -c $(dirname $0)/config.ini external2 &
+fi
