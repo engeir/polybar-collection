6d987bb0ddcf44266800155bc99ab4ee9e745915
engeir <engeir@pm.me>
2023-10-11 08:21:40 +0200

    feat(hideIt): hide all polybars completely
    If the peek is non-zero it messes with fullscreen mode. And if one
    monitor uses hideIt and one does not, the one that doesn't get a very
    poor fullscreen mode, where the bar is overlaid.

--- kneipp/launch.sh
+++ kneipp/launch.sh
@@ -11,24 +11,25 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar
 polybar top -c $(dirname $0)/config.ini & disown
-bspc config -m eDP-1 top_padding 22
-# sleep 0.5
-# hideIt.sh -w -i 0.2 -d "top" --name "polybar-top_eDP-1" --region 0x360+1920+22 & disown
-# sleep 0.5
+bspc config -m eDP-1 top_padding 0 # 22
 # polybar usbc -c $(dirname $0)/config.ini & disown
 # polybar -c ~/.config/polybar/config.ini external & disown
 
 my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
-if [[ $my_laptop_external_monitor = *connected* ]]; then
+if [[ $my_laptop_external_monitor = *" connected"* ]]; then
     polybar external -c $(dirname $0)/config.ini external & disown
 fi
 
 my_laptop_external_monitor=$(xrandr --query | grep 'DP-2')
-if [[ $my_laptop_external_monitor = *connected* ]]; then
+if [[ $my_laptop_external_monitor = *" connected"* ]]; then
     polybar usbc -c $(dirname $0)/config.ini & disown
-    bspc config -m DP-2 top_padding 2
+    bspc config -m DP-2 top_padding 0 # 2
     sleep 0.5
-    hideIt.sh -w -i 0.2 -d "top" --name "polybar-usbc_DP-2" --region 1920x0+2560+3 & disown
+    hideIt.sh -w -i 0.2 -d "top" --name "polybar-top_eDP-1" --peek 0 --region 0x360+1920+3 & disown
+    sleep 0.5
+    hideIt.sh -w -i 0.2 -d "top" --name "polybar-usbc_DP-2" --peek 0 --region 1920x0+2560+3 & disown
+else
+    hideIt.sh -w -i 0.2 -d "top" --name "polybar-top_eDP-1" --peek 0 --region 0x0+1920+22 & disown
 fi
 
 sleep 0.5
